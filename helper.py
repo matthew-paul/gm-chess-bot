dedb7bdddc866aced91414e73c6b3be43a188edd
matthew-paul <matthew.paul.mail@gmail.com>
2021-03-21 19:25:10 -0500

    computer does best move if player starts on white

--- helper.py
+++ helper.py
@@ -1,60 +1,160 @@
+import chess
 from PIL import ImageTk, Image
 
+colors = {'light_blue': '#8ad6ff',
+          'dark_blue': '#619eff',
+          'black': '#000',
+          'light_red': '#d14747',
+          'light_yellow': '#fffa75',
+          'white': '#fff'}
 
-def draw_pieces(tkCanvas, black, white, square_size):
-    black['rook1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rdt60.png'))
-    tkCanvas.create_image(39, 39, image=black['rook1'])
+square_map = {(0, 0): chess.A8,
+              (0, 1): chess.A7,
+              (0, 2): chess.A6,
+              (0, 3): chess.A5,
+              (0, 4): chess.A4,
+              (0, 5): chess.A3,
+              (0, 6): chess.A2,
+              (0, 7): chess.A1,
+              (1, 0): chess.B8,
+              (1, 1): chess.B7,
+              (1, 2): chess.B6,
+              (1, 3): chess.B5,
+              (1, 4): chess.B4,
+              (1, 5): chess.B3,
+              (1, 6): chess.B2,
+              (1, 7): chess.B1,
+              (2, 0): chess.C8,
+              (2, 1): chess.C7,
+              (2, 2): chess.C6,
+              (2, 3): chess.C5,
+              (2, 4): chess.C4,
+              (2, 5): chess.C3,
+              (2, 6): chess.C2,
+              (2, 7): chess.C1,
+              (3, 0): chess.D8,
+              (3, 1): chess.D7,
+              (3, 2): chess.D6,
+              (3, 3): chess.D5,
+              (3, 4): chess.D4,
+              (3, 5): chess.D3,
+              (3, 6): chess.D2,
+              (3, 7): chess.D1,
+              (4, 0): chess.E8,
+              (4, 1): chess.E7,
+              (4, 2): chess.E6,
+              (4, 3): chess.E5,
+              (4, 4): chess.E4,
+              (4, 5): chess.E3,
+              (4, 6): chess.E2,
+              (4, 7): chess.E1,
+              (5, 0): chess.F8,
+              (5, 1): chess.F7,
+              (5, 2): chess.F6,
+              (5, 3): chess.F5,
+              (5, 4): chess.F4,
+              (5, 5): chess.F3,
+              (5, 6): chess.F2,
+              (5, 7): chess.F1,
+              (6, 0): chess.G8,
+              (6, 1): chess.G7,
+              (6, 2): chess.G6,
+              (6, 3): chess.G5,
+              (6, 4): chess.G4,
+              (6, 5): chess.G3,
+              (6, 6): chess.G2,
+              (6, 7): chess.G1,
+              (7, 0): chess.H8,
+              (7, 1): chess.H7,
+              (7, 2): chess.H6,
+              (7, 3): chess.H5,
+              (7, 4): chess.H4,
+              (7, 5): chess.H3,
+              (7, 6): chess.H2,
+              (7, 7): chess.H1,
+              }
 
-    black['knight1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_ndt60.png'))
-    tkCanvas.create_image(square_size + 39, 39, image=black['knight1'])
 
-    black['bishop1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_bdt60.png'))
-    tkCanvas.create_image(square_size * 2 + 39, 39, image=black['bishop1'])
+# assign images
+black = dict()
+white = dict()
+created_images = False
 
+board_size = 600
+square_size = board_size // 8
+flipped = False
+
+
+def get_square(x, y):
+    square_x, square_y = (x - 3) // square_size, (y - 3) // square_size
+    if flipped:
+        square_y = 7 - square_y
+
+    return square_x, square_y
+
+
+class ChessPiece:
+    def __init__(self, canvas, color, piece_type, piece_x, piece_y, img):
+        self.color = color
+        self.type = piece_type
+        self.img = img
+        self.x = piece_x
+        self.y = piece_y
+        self.id = canvas.create_image(piece_x, piece_y, image=img)
+
+        self.square = square_map[get_square(self.x, self.y)]
+
+        return
+
+    def update_square(self):
+        self.square = square_map[get_square(self.x, self.y)]
+
+    def __str__(self):
+        return f'{self.color}, {self.type}, {self.square}'
+
+def create_images():
+    black['rook'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rdt60.png'))
+    black['knight'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_ndt60.png'))
+    black['bishop'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_bdt60.png'))
     black['queen'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_qdt60.png'))
-    tkCanvas.create_image(square_size * 3 + 39, 39, image=black['queen'])
-
     black['king'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_kdt60.png'))
-    tkCanvas.create_image(square_size * 4 + 39, 39, image=black['king'])
-
-    black['bishop2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_bdt60.png'))
-    tkCanvas.create_image(square_size * 5 + 39, 39, image=black['bishop2'])
-
-    black['knight2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_ndt60.png'))
-    tkCanvas.create_image(square_size * 6 + 39, 39, image=black['knight2'])
-
-    black['rook2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rdt60.png'))
-    tkCanvas.create_image(square_size * 7 + 39, 39, image=black['rook2'])
-
-    for i in range(8):
-        black[f'pawn{i}'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_pdt60.png'))
-        tkCanvas.create_image(square_size * i + 39, square_size + 39, image=black[f'pawn{i}'])
-
-
-    white['rook1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rlt60.png'))
-    tkCanvas.create_image(39, square_size * 7 + 39, image=white['rook1'])
-
-    white['knight1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_nlt60.png'))
-    tkCanvas.create_image(square_size + 39, square_size * 7 + 39, image=white['knight1'])
-
-    white['bishop1'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_blt60.png'))
-    tkCanvas.create_image(square_size * 2 + 39, square_size * 7 + 39, image=white['bishop1'])
-
+    black['pawn'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_pdt60.png'))
+    white['rook'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rlt60.png'))
+    white['knight'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_nlt60.png'))
+    white['bishop'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_blt60.png'))
     white['queen'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_qlt60.png'))
-    tkCanvas.create_image(square_size * 3 + 39, square_size * 7 + 39, image=white['queen'])
-
     white['king'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_klt60.png'))
-    tkCanvas.create_image(square_size * 4 + 39, square_size * 7 + 39, image=white['king'])
+    white['pawn'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_plt60.png'))
+    created_images = True
 
-    white['bishop2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_blt60.png'))
-    tkCanvas.create_image(square_size * 5 + 39, square_size * 7 + 39, image=white['bishop2'])
 
-    white['knight2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_nlt60.png'))
-    tkCanvas.create_image(square_size * 6 + 39, square_size * 7 + 39, image=white['knight2'])
+def draw_pieces(tkCanvas):
 
-    white['rook2'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_rlt60.png'))
-    tkCanvas.create_image(square_size * 7 + 39, square_size * 7 + 39, image=white['rook2'])
+    pieces = dict()
 
+    if not created_images:
+        create_images()
+
+    pieces['r1'] = ChessPiece(tkCanvas, 'black', 'r', 39, 39, black['rook'])
+    pieces['n1'] = ChessPiece(tkCanvas, 'black', 'n', square_size + 39, 39, black['knight'])
+    pieces['b1'] = ChessPiece(tkCanvas, 'black', 'b', square_size * 2 + 39, 39, black['bishop'])
+    pieces['q'] = ChessPiece(tkCanvas, 'black', 'q', square_size * 3 + 39, 39, black['queen'])
+    pieces['k'] = ChessPiece(tkCanvas, 'black', 'k', square_size * 4 + 39, 39, black['king'])
+    pieces['b2'] = ChessPiece(tkCanvas, 'black', 'b', square_size * 5 + 39, 39, black['bishop'])
+    pieces['n2'] = ChessPiece(tkCanvas, 'black', 'n', square_size * 6 + 39, 39, black['knight'])
+    pieces['r2'] = ChessPiece(tkCanvas, 'black', 'r', square_size * 7 + 39, 39, black['rook'])
     for i in range(8):
-        white[f'pawn{i}'] = ImageTk.PhotoImage(Image.open('resources/pieces/Chess_plt60.png'))
-        tkCanvas.create_image(square_size * i + 39, square_size * 6 + 39, image=white[f'pawn{i}'])
+        pieces[f'p{i}'] = ChessPiece(tkCanvas, 'black', 'p', square_size * i + 39, square_size + 39, black['pawn'])
+
+    pieces['R1'] = ChessPiece(tkCanvas, 'white', 'R', 39, square_size * 7 + 39, white['rook'])
+    pieces['N1'] = ChessPiece(tkCanvas, 'white', 'N', square_size + 39, square_size * 7 + 39, white['knight'])
+    pieces['B1'] = ChessPiece(tkCanvas, 'white', 'B', square_size * 2 + 39, square_size * 7 + 39, white['bishop'])
+    pieces['Q'] = ChessPiece(tkCanvas, 'white', 'Q', square_size * 3 + 39, square_size * 7 + 39, white['queen'])
+    pieces['K'] = ChessPiece(tkCanvas, 'white', 'K', square_size * 4 + 39, square_size * 7 + 39, white['king'])
+    pieces['B2'] = ChessPiece(tkCanvas, 'white', 'B', square_size * 5 + 39, square_size * 7 + 39, white['bishop'])
+    pieces['K2'] = ChessPiece(tkCanvas, 'white', 'N', square_size * 6 + 39, square_size * 7 + 39, white['knight'])
+    pieces['R2'] = ChessPiece(tkCanvas, 'white', 'R', square_size * 7 + 39, square_size * 7 + 39, white['rook'])
+    for i in range(8):
+        pieces[f'P{i}'] = ChessPiece(tkCanvas, 'white', 'P', square_size * i + 39, square_size * 6 + 39, white['pawn'])
+
+    return pieces
--- main.py
+++ main.py
@@ -1,33 +1,138 @@
 import chess
+from stockfish import Stockfish
 from tkinter import *
-from helper import draw_pieces
+from helper import *
 
 board = chess.Board()
-flipped = False
+
+selected_square = None
+selected_piece = None
+
+stockfish = Stockfish('C:/Users/matth/Documents/Chess/stockfish_13_win_x64_bmi2/stockfish_13_win_x64_bmi2/stockfish_13_win_x64_bmi2.exe', parameters={"Minimum Thinking Time": 1000})
 
 # functions
 
 
+def get_key(dictionary, val):
+    for key, value in dictionary.items():
+        if val == value:
+            return key
+    return None
+
+
+def get_piece_from_square(square):
+    for k, piece in pieces.items():
+        if piece.square == square:
+            return piece
+    return None
+
+
+def reset_colors():
+    for i in range(8):
+        for j in range(8):
+            canvas.itemconfig(squares[i][j], fill=colors['light_blue'] if (i + j) % 2 == 0 else colors['dark_blue'])
+
+
+def move(square1, square2):
+    global selected_square, selected_piece
+    selected_square = None
+    selected_piece = None
+
+    attempted_move = chess.Move(from_square=square1, to_square=square2)
+    board.push(attempted_move)
+
+    piece1 = get_piece_from_square(square1)
+    print(f'Piece to move: {piece1}')
+    piece2 = get_piece_from_square(square2)
+    old_x, old_y = piece1.x, piece1.y
+
+    if not piece2:
+        # move image to square 2
+        new_x, new_y = get_key(square_map, square2)
+        new_x = square_size * new_x + 39
+        new_y = square_size * new_y + 39
+        canvas.move(piece1.id, new_x - old_x, new_y - old_y)
+
+        # update piece 1
+        piece1.x = new_x
+        piece1.y = new_y
+        piece1.update_square()
+
+
+    else:
+        new_x, new_y = piece2.x, piece2.y
+        canvas.delete(piece2.id)
+        del pieces[get_key(pieces, piece2)]
+
+        # move image to square 2
+        canvas.move(piece1.id, new_x - old_x, new_y - old_y)
+
+        # update piece 1
+        piece1.x = new_x
+        piece1.y = new_y
+        piece1.update_square()
+
+    stockfish.set_fen_position(board.fen())
+
+
+def automove():
+    best_move = stockfish.get_best_move()
+    square_1_str, square_2_str = best_move[0:2], best_move[2:]
+    square_1_x, square_1_y = ord(square_1_str[0]) - ord('a'), 8 - int(square_1_str[1])
+    square_2_x, square_2_y = ord(square_2_str[0]) - ord('a'), 8 - int(square_2_str[1])
+
+    #print(f'({square_1_x}, {square_1_y})')
+    #print(f'({square_2_x}, {square_2_y})')
+
+    square1 = square_map[(square_1_x, square_1_y)]
+    square2 = square_map[(square_2_x, square_2_y)]
+
+
+
+    move(square1, square2)
+
+
+
 def mouse_clicked(eventorigin):
+    global selected_square, selected_piece
     mouse_x = eventorigin.x
     mouse_y = eventorigin.y
 
-    square_x, square_y = (mouse_x - 5) // square_size, (mouse_y - 5) // square_size
+    square_x, square_y = get_square(mouse_x, mouse_y)
 
-    print(mouse_x, mouse_y)
-    print(square_x, square_y)
+    if 0 <= square_x <= 7 and 0 <= square_y <= 7:
+
+        # no piece selected
+        if selected_square is None and get_piece_from_square(square_map[(square_x, square_y)]) is not None:
+            canvas.itemconfig(squares[square_x][square_y], fill=colors['light_yellow'])
+            selected_square = square_map[(square_x, square_y)]
+            selected_piece = get_piece_from_square(square_map[(square_x, square_y)])
+
+        # piece selected
+        else:
+            new_square = square_map[(square_x, square_y)]
+
+            # unselect if clicked again
+            if new_square == selected_square:
+                canvas.itemconfig(squares[square_x][square_y],
+                                  fill=colors['light_blue'] if (square_x + square_y) % 2 == 0 else colors['dark_blue'])
+            # attempt to move
+            elif selected_piece is not None:
+                # do move
+                move(selected_square, new_square)
+                automove()
+
+                # reset colors
+                reset_colors()
+
+            selected_square = None
+            selected_piece = None
 
 
 # gui stuff
-board_size = 600
-square_size = board_size // 8
-
-light_blue = '#8ad6ff'
-dark_blue = '#619eff'
-
 board_gui = Tk()
 board_gui.title("Chess Bot")
-board_gui.minsize(board_size + 5, board_size + 10)
+board_gui.minsize(board_size + 5, board_size + 5)
 board_gui.resizable(0, 0)
 
 frame = Frame(board_gui)
@@ -39,13 +144,14 @@ canvas.pack(fill=BOTH, expand=True)
 # draw squares
 squares = []
 
-
 toggle = flipped  # top left color based on whether board is flipped
 for x in range(2, board_size, square_size):
     toggle = not toggle
     square_row = []
     for y in range(2, board_size, square_size):
-        square_row.append(canvas.create_rectangle(x, y, x + square_size, y + square_size, fill=light_blue if toggle else dark_blue, outline='#000'))
+        square_row.append(canvas.create_rectangle(x, y, x + square_size, y + square_size,
+                                                  fill=colors['light_blue'] if toggle else colors['dark_blue'],
+                                                  outline=colors['white']))
         toggle = not toggle
         y += square_size
     squares.append(square_row)
@@ -53,14 +159,12 @@ for x in range(2, board_size, square_size):
 
 # draw pieces
 
-black_pieces = {}
-white_pieces = {}
-
-draw_pieces(canvas, black_pieces, white_pieces, square_size)
+pieces = draw_pieces(canvas)
 
 # bind mouse click
 board_gui.bind("<Button 1>", mouse_clicked)
 
-draw_pieces(canvas, black_pieces, white_pieces, square_size)
+
 board_gui.mainloop()
+
 
